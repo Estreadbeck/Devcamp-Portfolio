b6f82b86f6d6793518aeb395554dbaaf8c011c3b
Ethan Streadbeck <estreadbeck@gmail.com>
2018-01-30 11:33:44 -0700

    added partials for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -28,6 +28,7 @@ class PortfoliosController < ApplicationController
 
   def edit
   	@portfolio_item = Portfolio.find(params[:id])
+    3.times { @portfolio_item.technologies.build }
 
   end
 
@@ -53,7 +54,6 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.find(params[:id])
 
 		@portfolio_item.destroy
-    
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Successfully destroyed. For science. You monster.' }
       
