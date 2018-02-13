d667c3fea25526438fb3e6c2bde91ca73d481283
Ethan Streadbeck <estreadbeck@gmail.com>
2018-02-13 16:18:32 -0700

    completed comment feature with actioncable

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,18 @@
 module BlogsHelper
+  def gravatar_helper user
+    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 40
+  end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Ethan Streadbeck Portfolio", sticky: false)
+  end
 end
