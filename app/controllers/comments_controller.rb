993e33c01d04686cc8a444290e52602b5ddbb379
Ethan Streadbeck <estreadbeck@gmail.com>
2018-02-16 11:20:22 -0700

    Took out temporary CRUD functionality from comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,40 +8,40 @@ class CommentsController < ApplicationController
   end
 
   
-  def edit
-  end
+  # def edit
+  # end
 
-  def show
-    def show
-    if logged_in?(:site_admin) || @blog.published?
-      @blog = Blog.includes(:comments).friendly.find(params[:id])
-      @comment = Comment.new
+  # def show
+  #   def show
+  #   if logged_in?(:site_admin) || @blog.published?
+  #     @blog = Blog.includes(:comments).friendly.find(params[:id])
+  #     @comment = Comment.new
 
-      @page_title = @blog.title
-      @seo_keywords = @blog.body
-    else
-      redirect_to blogs_path, notice: "You are not authorized to access this page"
-    end
-  end
-  end
+  #     @page_title = @blog.title
+  #     @seo_keywords = @blog.body
+  #   else
+  #     redirect_to blogs_path, notice: "You are not authorized to access this page"
+  #   end
+  # end
+  # end
 
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to blogs_path, notice: 'Comment was successfully updated.' }
-      else
-        format.html { render :show }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @comment.update(comment_params)
+  #       format.html { redirect_to blogs_path, notice: 'Comment was successfully updated.' }
+  #     else
+  #       format.html { render :show }
+  #     end
+  #   end
+  # end
 
-  def destroy
-    @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to blogs_path, notice: 'Comment was removed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @comment.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to blogs_path, notice: 'Comment was removed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
    def set_comment
     @comment = Comment.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
-  resources :comments, except: [:index] do
-    put :sort, on: :collection
-  end
+  # resources :comments, except: [:index] do
+  #   put :sort, on: :collection
+  # end
 
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
