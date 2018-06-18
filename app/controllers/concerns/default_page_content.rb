9008d7ee1c9e92cf96dd99b12e7d113f55fca523
Ethan Streadbeck <estreadbeck@gmail.com>
2018-06-18 15:46:09 -0600

    Began overhauling of website for freelancing

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Ethan Streadbeck | My Portfolio Website"
-    @seo_keywords = "Ethan Streadbeck portfolio"
+    @page_title = "Streadbeck Development | Custom Websites"
+    @seo_keywords = "Ethan Streadbeck Development Custom Websites"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,10 @@ Rails.application.routes.draw do
   #   put :sort, on: :collection
   # end
 
-  get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  get 'tech-news', to: 'pages#tech_news'
-
     
   resources :blogs do
     member do
